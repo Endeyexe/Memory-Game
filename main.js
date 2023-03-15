4e62725857cca0c250a2bbc361df3fd760d3ea81
Endeyexe <108495952+Endeyexe@users.noreply.github.com>
2023-03-15 17:31:54 -0400

    new gamemodes

--- main.js
+++ main.js
@@ -1,34 +1,77 @@
 const colors = ["red", "blue", "green", "yellow","orange","purple","pink"]
-let answerList = []
+let squareList = []
+let numList = []
 const content = document.getElementById("content")
 const form = document.getElementById("form")
 const userAnswer = document.getElementById('answer')
-const squares = document.querySelectorAll('.square');
-
+const squares = document.querySelectorAll('.square')
+const modes = document.getElementById('modes')
+const buttons = modes.getElementsByTagName("button")
+let answer = ""
+let showNums = false
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function hideSquares(){
-    squares.forEach(square => square.style.backgroundColor = "white");
+    squares.forEach((square) => {
+        square.style.backgroundColor = "white"
+        square.innerHTML = ""
+    });
 }
-function showSquares(answerList){
-    for (let i=0; i<answerList.length; i++){
-        squares[i].style.backgroundColor = answerList[i];
 
+function showSquares(squareList, numList, showNums){
+    for (let i=0; i<squareList.length; i++){
+        squares[i].style.backgroundColor = squareList[i];
+        if(showNums){squares[i].innerHTML = numList[i]}
     }
 }
-async function startGame(viewTime){
-    for (let i=0; i<4; i++){
-        answerList.push(colors[Math.floor(Math.random()*colors.length)])
+
+function disableButtons() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+        console.log("works?")
     }
-    showSquares(answerList)
+}
+
+function enableButtons() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = false;
+    }
+}
+
+async function startGame(viewTime){
+    disableButtons()
+    for (let i=0; i<4; i++){
+        let color = colors[Math.floor(Math.random()*colors.length)]
+        squareList.push(color)
+        numList.push((Math.floor(Math.random()*10)).toString())
+    }
+    console.log(numList)
+    showSquares(squareList, numList, showNums)
     await sleep(viewTime)
     hideSquares()
     userAnswer.disabled = false
     userAnswer.focus()
-    answer = answerList.join(" ")
+    if (showNums) {
+        for (let i = 0; i < squareList.length; i++) {
+            answer += `${squareList[i]} ${numList[i]} `
+        }
+        console.log(answer)
+        answer = answer.trim() //to remove extra space at end
+        console.log(answer)
+    }
+    else{answer = squareList.join(" ")}
+}
+
+function reset(){
+    squareList = []
+    numList = []
+    answer = ""
+    userAnswer.disabled = true
+    content.style.backgroundColor = "#291429"
+    userAnswer.value = ""
 }
 
 form.addEventListener("submit", (e) => {
@@ -41,9 +84,18 @@ form.addEventListener("submit", (e) => {
         content.style.backgroundColor = "maroon"
         userAnswer.value = answer
     }
-    showSquares(answerList)
+    showSquares(squareList, numList, showNums)
     userAnswer.disabled = true
+    enableButtons()
 })
+function gamemode1(){
+    reset()
+    showNums = false
+    startGame(2000)
+}
 
-startGame(2000)
-
+function gamemode2(){
+    reset()
+    showNums = true
+    startGame(4000)
+}
