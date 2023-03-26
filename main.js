8b7f433a75346bdc997602df1631363439fca623
Endeyexe <108495952+Endeyexe@users.noreply.github.com>
2023-03-26 18:15:38 -0400

    new gamemode

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const colors = ["red", "blue", "green", "yellow","orange","purple","pink"]
+const shapes = ['square', 'triangle', 'star', 'circle']
 let boxList = []
 let numList = []
+let shapeList = []
 const content = document.getElementById("content")
 const form = document.getElementById("form")
 const userAnswer = document.getElementById('answer')
@@ -9,6 +11,7 @@ const modes = document.getElementById('modes')
 const buttons = modes.getElementsByTagName("button")
 let answer = ""
 let showNums = false
+let showShapes = false
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,13 +21,15 @@ function hideBoxes(){
     boxes.forEach((box) => {
         box.style.backgroundColor = "#250F25"
         box.innerHTML = ""
+        box.style.backgroundImage = "none";
     });
 }
 
-function showBoxes(boxList, numList, showNums){
+function showBoxes(){
     for (let i=0; i<boxList.length; i++){
         boxes[i].style.backgroundColor = boxList[i];
         if(showNums){boxes[i].innerHTML = numList[i]}
+        if(showShapes) {boxes[i].style.backgroundImage = `url(./shapes/${shapeList[i]}.png)`}
     }
 }
 
@@ -39,22 +44,30 @@ function enableButtons() {
         buttons[i].disabled = false;
     }
 }
-
-async function startGame(viewTime){
-    disableButtons()
+function randomizeValues(){
     for (let i=0; i<4; i++){
         let color = colors[Math.floor(Math.random()*colors.length)]
         boxList.push(color)
-        numList.push((Math.floor(Math.random()*10)).toString())
+        if(showNums) numList.push((Math.floor(Math.random()*10)).toString())
+        if(showShapes) {
+            let shape = shapes[Math.floor(Math.random()*shapes.length)]
+            shapeList.push(shape)
+        }
     }
-    showBoxes(boxList, numList, showNums)
+}
+
+async function startGame(viewTime){
+    disableButtons()
+    randomizeValues()
+    showBoxes()
     await sleep(viewTime)
     hideBoxes()
     userAnswer.disabled = false
     userAnswer.focus()
-    if (showNums) {
+    if (showShapes) {
         for (let i = 0; i < boxList.length; i++) {
-            answer += `${boxList[i]} ${numList[i]} `
+            answer += `${boxList[i]} ${shapeList[i]} `
+            if (showNums){answer += numList[i] += ' '}
         }
         answer = answer.trim() //to remove extra space at end
     }
@@ -64,6 +77,9 @@ async function startGame(viewTime){
 function reset(){
     boxList = []
     numList = []
+    shapeList = []
+    showNums = false
+    showShapes = false
     answer = ""
     userAnswer.disabled = true
     content.style.backgroundColor = "#291429"
@@ -91,6 +107,13 @@ function gamemode1(){
 
 function gamemode2(){
     reset()
-    showNums = true
+    showShapes = true;
     startGame(4000)
+}
+
+function gamemode3(){
+    reset()
+    showNums = true
+    showShapes = true
+    startGame(6000)
 }
